30289caecc76164ae722afcdc3dd5e0b966c1514
Yana Bozhko <yakji2005@gmail.com>
2024-10-02 06:09:13 +0300

    Day 1

---
+++ Test.MA/Test.MA/Program.cs
@@ -0,0 +1,79 @@
+﻿using Test.MA;
+
+Stroka str = new();
+
+var gena = new List<Stroka>();
+
+string[] files = new string[10];
+
+int n = 10;
+
+//var builder = new StringBuilder();
+
+string path = "a";
+
+string line;
+
+string paths = @"V:\\file.txt";
+
+StreamWriter sw1 = new(paths);
+sw1.Close();
+
+bool exist = false;
+
+int count = 0;
+try
+{
+    for (int o = 1; o <= n; o++)
+    {
+        StreamWriter sw = new(@"V:\\filename" + o + ".txt");
+        files[o-1] = "V:\\filename" + o + ".txt";
+        gena = str.Generate(1);
+        for (int i = 0; i < gena.Count; i++)
+        {
+            sw.WriteLine($"{gena[i].Date:d}||{gena[i].Latin}||{gena[i].Rus}||{gena[i].Num_Int}||{gena[i].Num_Float}");
+        }
+
+        sw.Close();
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine("Exception: " + e.Message);
+}
+
+foreach (var file in files)
+{
+    try
+    {
+        StreamReader sr = new(file);
+        if (!File.Exists(path))
+        {
+            exist = true;
+
+        }
+        StreamWriter sw2 = new(paths, exist);
+        line = sr.ReadLine();
+        while (line != null)
+        {
+            if (line.Contains(path) is false)
+            {
+                sw2.WriteLine($"{line}");
+
+            }
+            else count++;
+            line = sr.ReadLine();
+
+        }
+        sw2.WriteLine($"{count}");
+        sr.Close();
+        sw2.Close();
+        //builder.Append(File.ReadAllText(file));
+        //File.WriteAllText("V:\\fileOutput.txt", builder.ToString());
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Exception: " + e.Message);
+    }
+    
+}
---
+++ Test.MA/Test.MA/Stroka.cs
@@ -0,0 +1,50 @@
+﻿using Bogus;
+
+namespace Test.MA
+{
+    public class Stroka : IStroka
+    {
+        public DateTime Date { get; set; }
+        public string Latin { get; set; }
+        public string Rus { get; set; }
+        public int Num_Int { get; set; }
+        public double Num_Float { get; set; }
+
+        public List<Stroka> Generate(int n)
+        {
+            var generator = new Faker<Stroka>()
+             .StrictMode(true)
+             .RuleFor(x => x.Date, f => f.Date.Past(5))
+             .RuleFor(x => x.Latin, f => f.Random.String(1))
+             .RuleFor(x => x.Rus, f => f.Random.String(1))
+             .RuleFor(x => x.Num_Int, f => f.Random.Int(1, 100000000))
+             .RuleFor(x => x.Num_Float, f => f.Random.Int(1, 2000000000));
+
+            var gen = new List<Stroka>();
+
+            char[] alphabet = Enumerable.Range('a', 'z' - 'a' + 1)
+                                        .Select(i => (char)i)
+                                        .Concat(Enumerable.Range('A', 'Z' - 'A' + 1).Select(i => (char)i))
+                                        .ToArray();
+            char[] alphabet2 = Enumerable.Range('а', 'я' - 'а' + 1)
+                                        .Select(i => (char)i)
+                                        .Concat(Enumerable.Range('А', 'Я' - 'А' + 1).Select(i => (char)i))
+                                        .ToArray();
+
+            for (int i = 1; i <= n; i++)
+            {
+                var stroka = generator.Generate();
+                stroka.Latin = new string(Enumerable.Repeat(alphabet, 10)
+                                              .Select(s => s[Random.Shared.Next(s.Length)])
+                                              .ToArray());
+                stroka.Rus = new string(Enumerable.Repeat(alphabet2, 10)
+                                              .Select(s => s[Random.Shared.Next(s.Length)])
+                                              .ToArray());
+                stroka.Num_Float = Math.Round(stroka.Num_Float / 100000000, 8);
+                gen.Add(stroka);
+            }
+
+            return gen;
+        }
+    }
+}
